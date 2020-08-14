737f8016d4bb54b03414c496018942943c726dce
Roshan Elvitigala <roshanrox911@gmail.com>
2020-08-14 19:02:13 +1000

    added image route

--- server.js
+++ server.js
@@ -51,6 +51,37 @@ app.post('/register', (req,res)=>{
     res.json(database.users[database.users.length-1]);
    
 })
+
+app.put('/image', (req,res)=>{
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            user.entries++;
+            res.json(user.entries);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+   
+})
+
+app.get('/profile/:id', (req,res)=>{
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            res.json(user);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+})
+
 app.listen(3000, ()=>{
     console.log("app is running on port 3000");
 });
