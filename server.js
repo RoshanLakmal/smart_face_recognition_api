ed39e51f658f23fdf25083ef79d381d15a90ce65
Roshan Elvitigala <roshanrox911@gmail.com>
2020-08-20 23:05:57 +1000

    fix register return issue

--- server.js
+++ server.js
@@ -55,7 +55,6 @@ app.post('/register', (req,res)=>{
             id:'1',
             name: name,
             email: email,
-            password: password,
             entries: 0,
             joined: new Date()
     });
